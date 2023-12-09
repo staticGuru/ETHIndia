4a0a55d2e4b1cf46323c9ce0a658c4dbbbbf04bf
staticGuru <vigneshguru274@gmail.com>
2023-12-09 09:29:27 +0530

    Add the accesstoken creation in api sections

---
+++ src/api.ts
@@ -0,0 +1,27 @@
+import { AccessToken, Role } from '@huddle01/server-sdk/auth';
+const apiKey = "PZW2UTmHvqppR6IPTYzZ0yi5yRwKgYkN";
+
+const accessToken = new AccessToken({
+     apiKey,
+     roomId: "ujt-ckug-nzo",
+     role:  Role.HOST,
+     permissions: {
+       admin: true,
+       canConsume: true,
+       canProduce: true,
+       canProduceSources: {
+         cam: true,
+         mic: true,
+         screen: true,
+       },
+       canRecvData: true,
+       canSendData: true,
+       canUpdateMetadata: true,
+     },
+     options: {
+       metadata: { 
+          // you can add any custom attributes here which you want to associate with the user
+         walletAddress: "axit.eth"
+       },
+     },
+   });
