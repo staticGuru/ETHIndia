ab9c3ab060fa8a24b9162e031a7ca9eae0b73be5
staticGuru <vigneshguru274@gmail.com>
2023-12-09 09:31:16 +0530

    add the huddle provider in index sectioins

--- src/index.js
+++ src/index.js
@@ -1,14 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
+import { StateContextProvider } from "./context";
+import { HuddleProvider, HuddleClient } from "@huddle01/react";
+const PROJECT_ID = "971ZK9nBUG_gduwudbicyZg3YMiY85_1";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const huddleClient = new HuddleClient({
+  projectId: PROJECT_ID,
+  options: {
+    activeSpeakers: {
+      size: 8,
+    },
+  },
+});
+console.log("chainId", ChainId);
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <ThirdwebProvider desiredChainId={ChainId.Mumbai}>
+    <HuddleProvider key="huddle01-provider" client={huddleClient}>
+      <StateContextProvider>
+        <App />
+      </StateContextProvider>
+    </HuddleProvider>
+  </ThirdwebProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
